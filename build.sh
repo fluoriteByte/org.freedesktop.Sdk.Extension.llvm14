421cc43734e5027ad6d220c7b7bfd4a9967af31f
illy <fluoriteByte@proton.me>
2024-12-20 19:45:31 +0100

    rereev

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/env bash
-flatpak run org.flatpak.Builder build --repo=repo gay.pages.fluoriteByte.Sdk.Extensions.llvm14.yml --force-clean --disable-rofiles-fuse --ccache --user --verbose
+flatpak run org.flatpak.Builder build gay.pages.fluoriteByte.Sdk.Extensions.llvm14.yml --install --force-clean --disable-rofiles-fuse --ccache --user --verbose
