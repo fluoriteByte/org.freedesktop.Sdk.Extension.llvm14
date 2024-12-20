2092036242f1bf00274301bb9b53fef908ea07b6
illy <fluoriteByte@proton.me>
2024-12-20 18:55:09 +0100

    .

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/env bash
-flatpak run org.flatpak.Builder build --repo=repo gov.nasa.ikos.Ikos.yml --force-clean --disable-rofiles-fuse --ccache --user --verbose
+flatpak run org.flatpak.Builder build --repo=repo gay.pages.fluoriteByte.Sdk.Extensions.llvm14.yml --force-clean --disable-rofiles-fuse --ccache --user --verbose
